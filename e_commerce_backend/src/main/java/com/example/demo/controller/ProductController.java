c24941885e3115662ca482536379c76816a7a835
Devansh Akruvala <devanshakruvala@gmail.com>
2023-01-10 18:19:39 +0530

    List Products API Created

--- e_commerce_backend/src/main/java/com/example/demo/controller/ProductController.java
+++ e_commerce_backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,4 +43,9 @@ public class ProductController {
 		return new ResponseEntity<>(new ApiResponse(true, product),HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/list")
+	public ResponseEntity<List<ProductDto>> listProducts(){
+		List<ProductDto> products =  productService.getAllProducts();
+		return new ResponseEntity<>(products,HttpStatus.OK);
+	}
 }
--- e_commerce_backend/src/main/java/com/example/demo/service/ProductService.java
+++ e_commerce_backend/src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +29,46 @@ public class ProductService {
 	}
 	
 	
+	public List<ProductDto> getAllProducts(){
+		List<Product> products=productRepository.findAll();
+		List<ProductDto> allProducts=new ArrayList<>();
+		products.forEach(product -> {
+			allProducts.add(productToPoroductDto(product));
+		});
+		return allProducts;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	private ProductDto productToPoroductDto(Product product) {
+		ProductDto productDto = new ProductDto();
+		productDto.setProductName(product.getProductName());
+		productDto.setProductDescription(		product.getProductDescription());
+		productDto.setProductImageUrl(		product.getProductImageUrl());
+		productDto.setProductPrice(		product.getProductPrice());
+		productDto.setCategoryId(product.getCategory().getCategoryId());
+		return productDto;
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	//	private Product productDtoToProduct(ProductDto productDto) {
+//		Product product = new Product();
+//		product.setProductName(productDto.getProductName());
+//		product.setProductDescription(productDto.getProductDescription());
+//		product.setProductImageUrl(productDto.getProductImageUrl());
+//		product.setProductPrice(productDto.getProductPrice());
+//		product.setCategory(category);
+//		
+//	}
 }
