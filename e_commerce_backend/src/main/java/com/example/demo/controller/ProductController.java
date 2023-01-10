15f8da5f9d33a2cf4b8c0912ee1214bf7451d129
Devansh Akruvala <devanshakruvala@gmail.com>
2023-01-10 19:34:51 +0530

    Update Product Api

--- e_commerce_backend/src/main/java/com/example/demo/controller/ProductController.java
+++ e_commerce_backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,5 +49,24 @@ public class ProductController {
 	public ResponseEntity<List<ProductDto>> listProducts(){
 		List<ProductDto> products =  productService.getAllProducts();
 		return new ResponseEntity<>(products,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update/{productId}")
+	public ResponseEntity<ApiResponse> updateProduct(@PathVariable("productId") Integer productId, 
+			@RequestBody ProductDto productDto){
+		// every product has category id
+		// so we chage if its valid or not 
+		Optional<Category> optionalCategory= categoryService.getCategoryById(productDto.getCategoryId());
+		if(!optionalCategory.isPresent()) {
+			return new ResponseEntity<>(new ApiResponse(false,"Invalid Category Id"),HttpStatus.BAD_REQUEST);
+		}
+		Product product = null;
+		try {
+			product = productService.updateProduct(productDto,optionalCategory.get(),productId);
+		} catch (Exception e) {
+			
+			new ResponseEntity<>(new ApiResponse(false, e),HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(new ApiResponse(true, product),HttpStatus.OK);
 	}
 }
--- e_commerce_backend/src/main/java/com/example/demo/service/ProductService.java
+++ e_commerce_backend/src/main/java/com/example/demo/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,53 +17,57 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
-	public Product createProduct(ProductDto productDto,Category category) {
+
+	public Product createProduct(ProductDto productDto, Category category) {
 		Product product = new Product();
 		product.setProductName(productDto.getProductName());
 		product.setProductDescription(productDto.getProductDescription());
 		product.setProductImageUrl(productDto.getProductImageUrl());
 		product.setProductPrice(productDto.getProductPrice());
 		product.setCategory(category);
-		
+
 		return productRepository.save(product);
 	}
-	
-	
-	public List<ProductDto> getAllProducts(){
-		List<Product> products=productRepository.findAll();
-		List<ProductDto> allProducts=new ArrayList<>();
+
+	public List<ProductDto> getAllProducts() {
+		List<Product> products = productRepository.findAll();
+		List<ProductDto> allProducts = new ArrayList<>();
 		products.forEach(product -> {
 			allProducts.add(productToPoroductDto(product));
 		});
 		return allProducts;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 	private ProductDto productToPoroductDto(Product product) {
 		ProductDto productDto = new ProductDto();
 		productDto.setProductName(product.getProductName());
-		productDto.setProductDescription(		product.getProductDescription());
-		productDto.setProductImageUrl(		product.getProductImageUrl());
-		productDto.setProductPrice(		product.getProductPrice());
+		productDto.setProductDescription(product.getProductDescription());
+		productDto.setProductImageUrl(product.getProductImageUrl());
+		productDto.setProductPrice(product.getProductPrice());
 		productDto.setCategoryId(product.getCategory().getCategoryId());
+		productDto.setProductId(product.getProductId());
 		return productDto;
 	}
-	
 
-	
-	
-	
-	
-	
-	
-	//	private Product productDtoToProduct(ProductDto productDto) {
+	public Product updateProduct(ProductDto productDto, Category category, Integer productId) throws Exception {
+
+		Optional<Product> optionalProduct = productRepository.findById(productId);
+
+		if (!optionalProduct.isPresent()) {
+			throw new Exception("product Not Present");
+		}
+		Product product = optionalProduct.get();
+		product.setProductName(productDto.getProductName());
+		product.setProductDescription(productDto.getProductDescription());
+		product.setProductImageUrl(productDto.getProductImageUrl());
+		product.setProductPrice(productDto.getProductPrice());
+		product.setCategory(category);
+		product.setProductId(productId);
+		Product updatedProduct = productRepository.save(product);
+		return updatedProduct;
+	}
+
+	// private Product productDtoToProduct(ProductDto productDto) {
 //		Product product = new Product();
 //		product.setProductName(productDto.getProductName());
 //		product.setProductDescription(productDto.getProductDescription());
