cf1ba0a074153e1150d7755ce3c23523fdf32d50
Devansh Akruvala <devanshakruvala@gmail.com>
2023-01-11 19:46:20 +0530

    Delete from cart API

--- e_commerce_backend/src/main/java/com/example/demo/controller/CartController.java
+++ e_commerce_backend/src/main/java/com/example/demo/controller/CartController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,4 +47,15 @@ public class CartController {
 		CartDto cartDto = cartService.getCartItems(user);
 		return new ResponseEntity<CartDto>(cartDto,HttpStatus.OK);
 	}
+	
+	@DeleteMapping("/delete/{cartItemId}")
+	public ResponseEntity<ApiResponse> deleteItem(@PathVariable("cartItemId") Integer cartItemId,
+			@RequestParam("token") String token){
+		User user = authenticationService.getUser(token);
+		cartService.deleteItemFromCart(cartItemId,user);
+		
+		return new ResponseEntity<>(new ApiResponse(true, "Item Deleted"),HttpStatus.OK);
+
+	}
+	
 }
--- e_commerce_backend/src/main/java/com/example/demo/service/CartService.java
+++ e_commerce_backend/src/main/java/com/example/demo/service/CartService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.dto.AddToCartDto;
 import com.example.demo.dto.CartDto;
 import com.example.demo.dto.CartItemDto;
+import com.example.demo.exceptions.CustomExceptions;
 import com.example.demo.model.Cart;
 import com.example.demo.model.Product;
 import com.example.demo.model.User;
@@ -51,6 +52,20 @@ public class CartService {
 		cartDto.setTotalCost(totalCost);
 		cartDto.setCartItem(cartItemDtos);
 		return cartDto;
+	}
+
+	public void deleteItemFromCart(Integer cartItemId, User user) {
+		
+		Optional<Cart> optionalCart = cartRepository.findById(cartItemId);
+		if(optionalCart.isEmpty()) {
+			throw new CustomExceptions("cart item id is invalid");
+		}
+		
+		Cart cart = optionalCart.get();
+		if(cart.getUser()!=user) {
+			throw new CustomExceptions("Cart doesnot belong to user");
+		}
+		cartRepository.delete(cart);
 	}
 	
 	
